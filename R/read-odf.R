6a31f9952f4f730aea5e645757c3e3e4df87b1fa
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2020-11-25 19:05:22 -0400

    with read_odf()

--- R/read-odf.R
+++ R/read-odf.R
@@ -1,5 +1,91 @@
 
-read_odf_header <- function(file, n_header = 1000) {
+#' Read 'ODF' files
+#'
+#' ODF (Ocean Data) files are whitespace-delimited files
+#' whose column names and are specified in a loosely formatted header.
+#'
+#' @param file A file, URL, or connection. Files ending in .gz, .bz2, .xz, or
+#'   .zip will be automatically uncompressed; URLs will be automatically
+#'   downloaded. See [readr::read_lines()] for a full description of how
+#'   this parameter is interpreted.
+#' @param skip Number of non-header rows to skip.
+#' @param n_max Maximum number of rows to read.
+#' @param header A previously read value obtained from [read_odf_header()].
+#' @param header_lines A previously read value obtained from
+#'   [read_odf_header_lines()].
+#' @param n_header The starting guess for number of header lines.
+#'
+#' @export
+#'
+read_odf <- function(file, skip = 0, n_max = Inf) {
+  header_lines <- read_odf_header_lines(file)
+  header <- read_odf_header(file, header_lines)
+  col_header <- unname(header[names(header) == "PARAMETER_HEADER"])
+
+  col_names <- vapply(col_header, "[[", "CODE", FUN.VALUE = character(1))
+
+  type_names <- vapply(col_header, "[[", "TYPE", FUN.VALUE = character(1))
+  readr_types <- list(
+    DOUB = readr::col_double(),
+    INTE = readr::col_integer(),
+    SYTM = readr::col_datetime("%d-%b-%Y %H:%M:%OS")
+  )[type_names]
+  names(readr_types) <- col_names
+  col_types <- do.call(readr::cols, readr_types[!vapply(readr_types, is.null, logical(1))])
+
+  readr::read_delim(
+    file, delim = " ", quote = "'", trim_ws = TRUE,
+    col_names = col_names,
+    col_types = col_types,
+    skip = length(header_lines) + 1 + skip,
+    n_max = n_max
+  )
+}
+
+#' @rdname read_odf
+#' @export
+read_odf_colmeta <- function(file, header = read_odf_header(file)) {
+  params <- lapply(header[names(header) == "PARAMETER_HEADER"], tibble::as_tibble)
+  params_tbl <- vctrs::vec_rbind(!!! unname(params), .names_to = "index")
+  params_tbl[-1] <- lapply(params_tbl[-1], readr::parse_guess)
+  params_tbl
+}
+
+#' @rdname read_odf
+#' @export
+read_odf_header <- function(file, header_lines = read_odf_header_lines(file)) {
+  # extract components
+  components <- stringr::str_match(
+    header_lines,
+    "^(\\s*)([A-Za-z0-9_]+\\s*=\\s*)?'?\\s*(.*?)\\s*'?\\s*,?\\s*$"
+  )
+
+  whitespace <- components[, 2]
+  name <- stringr::str_remove(components[, 3], "\\s*=\\s*$")
+  name[is.na(name)] <- ""
+  value <- components[, 4]
+
+  # model with two levels of whitespace: one for top level headers
+  # one for everything else
+  n_whitespace <- stringr::str_length(whitespace)
+  is_top_header <- (n_whitespace == min(n_whitespace)) & (header_lines != whitespace)
+  top_headers <- value[is_top_header]
+  which_top_header <- cumsum(is_top_header)
+
+  parsed <- rep(list(NULL), length(top_headers))
+  for (i in seq_along(top_headers)) {
+    value_i <- value[which_top_header == i][-1]
+    names(value_i) <- name[which_top_header == i][-1]
+    parsed[[i]] <- collapse_by_name(value_i)
+  }
+
+  names(parsed) <- top_headers
+  parsed
+}
+
+#' @rdname read_odf
+#' @export
+read_odf_header_lines <- function(file, n_header = 1000) {
   stopifnot(n_header > 0)
 
   lines <- readr::read_lines(file, n_max = n_header)
@@ -16,6 +102,24 @@ read_odf_header <- function(file, n_header = 1000) {
   }
 
   lines[seq_len(which(end_header)[1] - 1)]
+}
+
+#' @rdname read_odf
+#' @export
+odf_guess <- function() {
+  structure(list(), class = "oceandf_guess")
+}
+
+collapse_by_name <- function(x) {
+  out_names <- unique(names(x))
+  out <- rep(list(NULL), length(out_names))
+  for (i in seq_along(out_names)) {
+    items <- unname(x[names(x) == out_names[i]])
+    out[[i]] <- items
+  }
+
+  names(out) <- out_names
+  out
 }
 
 test_odf_file <- function() {
--- tests/testthat/test-read-odf.R
+++ tests/testthat/test-read-odf.R
@@ -3,17 +3,41 @@ test_that("read_odf_header() works", {
   skip_if_not(has_test_odf())
 
   file <- test_odf_file()
-  header <- read_odf_header(file)
+  header <- read_odf_header_lines(file)
   expect_length(header, 881)
   expect_true(readr::read_lines(file, skip = 881, n_max = 1) == "-- DATA --")
 
   # force detection of header size
-  header <- read_odf_header(file, n_header = 10)
+  header <- read_odf_header_lines(file, n_header = 10)
   expect_length(header, 881)
 
   # invalid file
   tmpfile <- tempfile()
   write("totally bogus information", tmpfile)
-  expect_error(read_odf_header(tmpfile), "Can't find")
+  expect_error(read_odf_header_lines(tmpfile), "Can't find")
   unlink(tmpfile)
+})
+
+test_that("read_odf_header() works", {
+  skip_if_not(has_test_odf())
+
+  file <- test_odf_file()
+  header <- read_odf_header(file)
+  expect_true(all(c("PARAMETER_HEADER", "ODF_HEADER") %in% names(header)))
+})
+
+test_that("read_odf_colmeta() works", {
+  skip_if_not(has_test_odf())
+
+  file <- test_odf_file()
+  colmeta <- read_odf_colmeta(file)
+  expect_is(colmeta, "tbl_df")
+})
+
+test_that("read_odf() works", {
+  skip_if_not(has_test_odf())
+
+  file <- test_odf_file()
+  tbl <- read_odf(file)
+  expect_is(tbl$SYTM_01, "POSIXct")
 })
