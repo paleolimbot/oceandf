814091a54871c0006f98674238f806dfcb431550
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-03-01 15:29:51 -0400

    slightly more robust odf guessing

--- R/read-odf.R
+++ R/read-odf.R
@@ -8,63 +8,111 @@
 #'   .zip will be automatically uncompressed; URLs will be automatically
 #'   downloaded. See [readr::read_lines()] for a full description of how
 #'   this parameter is interpreted.
-#' @param skip_data Number of non-header rows to skip.
 #' @param n_max Maximum number of rows to read.
 #' @param header A previously read value obtained from [read_odf_header()].
 #' @param header_lines A previously read value obtained from
 #'   [read_odf_header_lines()].
+#' @param parameter_header A previously read value obtained from
+#'   [read_odf_parameter_header()].
+#' @param col_names A vector of column names or `NULL` to guess
+#'   using [odf_guess_col_names()]
+#' @param col_types A [readr::cols()] spec or `NULL` to guess
+#'   using [odf_guess_col_types()]
 #' @param n_header The starting guess for number of header lines.
+#' @param file_encoding The encoding used to encode the file. The default
+#'   (windows-1252) reflects a guess based on a number of example ODF files.
 #'
 #' @export
 #'
 #' @examples
 #' odf_file <- odf_example("CTD_98911_10P_11_DN.ODF")
 #' read_odf(odf_file)
+#'
+#' odf_guess_col_names(odf_file)
+#' odf_guess_col_types(odf_file)
 #' read_odf_parameter_header(odf_file)
+#'
+#' str(read_odf_header_lines(odf_file))
 #' str(read_odf_header(odf_file))
 #'
-read_odf <- function(file, skip_data = 0, n_max = Inf) {
-  header_lines <- read_odf_header_lines(file)
+read_odf <- function(file, col_names = NULL, col_types = NULL,
+                     n_max = -1,
+                     file_encoding = "windows-1252") {
+  header_lines <- read_odf_header_lines(file, file_encoding = file_encoding)
   header <- read_odf_header(file, header_lines)
-  col_header <- header$PARAMETER_HEADER
+  parameter_header <- read_odf_parameter_header(header = header)
 
-  col_names <- vapply(col_header, "[[", "NAME", FUN.VALUE = character(1))
+  col_names <- col_names %||%
+    odf_guess_col_names(parameter_header = parameter_header)
+  col_types <- col_types %||%
+    odf_guess_col_types(parameter_header = parameter_header, col_names = col_names)
 
-  type_names <- vapply(col_header, "[[", "TYPE", FUN.VALUE = character(1))
-  readr_types <- list(
-    DOUB = readr::col_double(),
-    INTE = readr::col_integer(),
-    SYTM = readr::col_datetime("%d-%b-%Y %H:%M:%OS")
-  )[type_names]
-  names(readr_types) <- col_names
-  col_types <- do.call(readr::cols, readr_types[!vapply(readr_types, is.null, logical(1))])
+  # can't use readr::read_table() because quoting is not supported
+  tbl <- utils::read.table(
+    file,
+    colClasses = "character",
+    col.names = col_names,
+    stringsAsFactors = FALSE,
+    check.names = FALSE,
+    skip = length(header_lines) + 1,
+    nrows = n_max, fileEncoding = file_encoding
+  )
 
-  readr::read_fwf(
-    file, trim_ws = TRUE,
-    col_positions = readr::fwf_empty(
-      file,
-      col_names = col_names,
-      skip = length(header_lines) + 1 + skip_data,
-      n = 100L
-    ),
-    col_types = col_types,
-    skip = length(header_lines) + 1 + skip_data,
-    n_max = n_max,
+  tibble::as_tibble(
+    readr::type_convert(tbl, col_types = col_types)
   )
 }
 
 #' @rdname read_odf
 #' @export
-read_odf_parameter_header <- function(file, header = read_odf_header(file)) {
+odf_guess_col_names <- function(file,
+                                parameter_header = read_odf_parameter_header(
+                                  file,
+                                  file_encoding = file_encoding
+                                ),
+                                file_encoding = "windows-1252") {
+  parameter_header$NAME
+}
+
+#' @rdname read_odf
+#' @export
+odf_guess_col_types <- function(file,
+                                col_names = odf_guess_col_names(parameter_header = parameter_header),
+                                parameter_header = read_odf_parameter_header(
+                                  file,
+                                  file_encoding = file_encoding
+                                ),
+                                file_encoding = "windows-1252") {
+  type_names <- parameter_header$TYPE
+
+  readr_types <- list(
+    DOUB = readr::col_double(),
+    INTE = readr::col_integer(),
+    SYTM = readr::col_datetime("%d-%b-%Y %H:%M:%OS")
+  )[type_names]
+
+  readr_type_null <- vapply(readr_types, is.null, logical(1))
+  readr_types[readr_type_null] <- list(readr::col_guess())
+  names(readr_types) <- col_names
+  do.call(readr::cols, readr_types)
+}
+
+#' @rdname read_odf
+#' @export
+read_odf_parameter_header <- function(file,
+                                      header = read_odf_header(file, file_encoding = file_encoding),
+                                      file_encoding = "windows-1252") {
   params <- lapply(header$PARAMETER_HEADER, tibble::as_tibble)
-  params_tbl <- vctrs::vec_rbind(!!! unname(params), .names_to = "index")
-  params_tbl[-1] <- lapply(params_tbl[-1], readr::parse_guess)
+  params_tbl <- vctrs::vec_rbind(!!! params)
+  params_tbl[] <- lapply(params_tbl, readr::parse_guess)
   params_tbl
 }
 
 #' @rdname read_odf
 #' @export
-read_odf_header <- function(file, header_lines = read_odf_header_lines(file)) {
+read_odf_header <- function(file,
+                            header_lines = read_odf_header_lines(file, file_encoding = file_encoding),
+                            file_encoding = "windows-1252") {
   # extract components
   components <- stringr::str_match(
     header_lines,
@@ -96,11 +144,13 @@ read_odf_header <- function(file, header_lines = read_odf_header_lines(file)) {
 
 #' @rdname read_odf
 #' @export
-read_odf_header_lines <- function(file, n_header = 1000) {
+read_odf_header_lines <- function(file, n_header = 1000,
+                                  file_encoding = "windows-1252") {
   header_lines(
     file,
     function(x) grepl("\\s*-- DATA --\\s*", x),
-    n_header = n_header
+    n_header = n_header,
+    file_encoding = file_encoding
   )
 }
 
--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,22 @@
 
-header_lines <- function(file, end_header_function, n_header = 100) {
+header_lines <- function(file, end_header_function, n_header = 100,
+                         file_encoding = "UTF-8") {
   stopifnot(n_header > 0)
 
-  lines <- readr::read_lines(file, n_max = n_header)
+  lines <- readr::read_lines(
+    file,
+    n_max = n_header,
+    locale = readr::locale(encoding = file_encoding)
+  )
   end_header <- end_header_function(lines)
 
   while ((length(lines) == n_header) && !any(end_header)) {
     n_header <- n_header * 2
-    lines <- readr::read_lines(file, n_max = n_header)
+    lines <- readr::read_lines(
+      file,
+      n_max = n_header,
+      locale = readr::locale(encoding = file_encoding)
+    )
     end_header <- end_header_function(lines)
   }
 
